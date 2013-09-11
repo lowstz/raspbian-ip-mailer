85fe62387651453a69e8a3e1804dbf8063c2c4c0
Lowstz Chen <lowstz@gmail.com>
2013-09-11 19:30:59 +0800

    refactor code

--- deploy.sh
+++ deploy.sh
@@ -3,21 +3,21 @@
 . /lib/lsb/init-functions
 
 # Settings
-SCRIPT_FILE=raspbian-ip-mailer.py
+SCRIPT_FILE=server-ip-mailer.py
 SCRIPT_PATH=/usr/local/bin
-LAUNCHER_FILE=raspbian-ip-mailer
+LAUNCHER_FILE=server-ip-mailer
 LAUNCHER_PATH=/etc/init.d
 
-log_action_msg Installing [ raspbian-ip-mailer ] \( by zshenjia@gmail.com \) ...
+log_action_msg Installing [ server-ip-mailer ] \( by zshenjia@gmail.com \) ...
 
 # Download script
 log_begin_msg Downloading [ $SCRIPT_FILE ] from Github to [ $SCRIPT_PATH ]...
-sudo wget https://raw.github.com/shenjia/raspbian-ip-mailer/master/$SCRIPT_FILE -O $SCRIPT_PATH/$SCRIPT_FILE -o /dev/null
+sudo wget https://raw.github.com/lowstz/server-ip-mailer/master/$SCRIPT_FILE -O $SCRIPT_PATH/$SCRIPT_FILE -o /dev/null
 log_end_msg $?
 
 # Download laucher
 log_begin_msg Downloading [ $LAUNCHER_FILE ] from Github to [ $LAUNCHER_PATH ]...
-sudo wget https://raw.github.com/shenjia/raspbian-ip-mailer/master/$LAUNCHER_FILE -O $LAUNCHER_PATH/$LAUNCHER_FILE  -o /dev/null
+sudo wget https://raw.github.com/lowstz/server-ip-mailer/master/$LAUNCHER_FILE -O $LAUNCHER_PATH/$LAUNCHER_FILE  -o /dev/null
 log_end_msg $?
 
 # Deploy
@@ -28,4 +28,3 @@ log_end_msg $?
 
 # Done
 log_action_msg Done! Don\'t forget to setup your Gmail account in \[ $SCRIPT_PATH/$SCRIPT_FILE \]
-
---
+++ server-ip-mailer.py
@@ -0,0 +1,108 @@
+#!/usr/bin/env python
+import os
+import subprocess
+import smtplib
+import socket
+import datetime
+import time
+from email.mime.text import MIMEText
+
+
+def start(action):
+    os.system('. /lib/lsb/init-functions; log_begin_msg "' + action + ' ..."')
+
+
+def success():
+    os.system('. /lib/lsb/init-functions; log_progress_msg done; log_end_msg 0')
+
+
+def fail():
+    os.system('. /lib/lsb/init-functions; log_end_msg 1')
+
+
+def get_hostname():
+    return socket.gethostname()
+
+
+def get_ipaddr():
+    p = subprocess.Popen('ip route list', shell=True, stdout=subprocess.PIPE)
+    data = p.communicate()
+    split_data = data[0].split()
+    ipaddr = split_data[split_data.index('src') + 1]
+    return ipaddr
+
+
+
+def sendmail(smtp_setting, mail_setting, send_list):
+    """
+    Connect to smtp server, try several times, if smtp servers were
+    successfully connected, then send the mail.
+    """
+    start('Connect to [' + smtp_setting['smtp_server'] + ']')
+    try_max = 5
+    try_times = 0
+    try_delay = 1
+    while try_times <= try_max:
+        try_times += 1
+        try:
+            smtpserver = smtplib.SMTP(smtp_setting['smtp_server'],
+                                      smtp_setting['smtp_port'])
+            success()
+            break
+        except Exception, err:
+            if try_times > try_max:
+                fail()
+                exit()
+            else:
+                time.sleep(try_delay)
+                try_delay *= 2
+
+    # Login to mail system
+    start('Login with ( ' + mail_setting['mail_user'] + ' )')
+    try:
+        smtpserver.ehlo()
+        smtpserver.starttls()
+        smtpserver.login(mail_setting['mail_user'],
+                         mail_setting['mail_password'])
+        success()
+    except Exception, err:
+        fail()
+        exit()
+
+    today = datetime.date.today()
+    ipaddr = get_ipaddr()
+    my_ip = 'Your ip is %s' % get_ipaddr()
+    hostname = "The host %s" % get_hostname()
+    start('Send ip mail ( ' + ipaddr + ' )')
+    msg = MIMEText(hostname + '\n' + my_ip)
+    msg['Subject'] = 'IP For' + hostname + 'on %s' % today.strftime('%b %d %Y')
+    msg['From'] = mail_setting['mail_user']
+    for send_user in send_list:
+        msg['To'] = send_user
+        try:
+            smtpserver.sendmail(mail_setting['mail_user'],
+                                [send_user], msg.as_string())
+            success()
+        except Exception, err:
+            fail()
+    smtpserver.quit()
+
+if __name__ == '__main__':
+    # Mail server settings
+    smtp_setting = {
+        'smtp_server': 'smtp.gmail.com',
+        'smtp_port': 587
+        }
+
+    mail_setting = {
+        'mail_user': 'username@gmail.com',
+        'mail_password': 'password'
+        }
+
+    # Mail account settings
+    send_to_list = [
+        'username@gmail.com',
+        'username2@gmail.com'
+        ]
+
+    sendmail(smtp_setting, mail_setting, send_to_list)
